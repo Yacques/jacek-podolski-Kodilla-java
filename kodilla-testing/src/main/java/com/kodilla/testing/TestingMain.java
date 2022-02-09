b865b4f8caeabe1831189b53348f7bd4b702592a
Jacek Podolski <jacekpodolski80@gmail.com>
2022-02-09 12:56:07 +0100

    6.2 Add kodilla-testing module

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,29 @@
 package com.kodilla.testing;
+import com.kodilla.testing.calculator.Calculator;
 
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania.");
+        Calculator tester = new Calculator();
+
+        int a = 15;
+        int b = 7;
+        int result1 = tester.add(a, b);
+
+        if(result1 == 22) {
+
+            System.out.println("Test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        int x = 30;
+        int y = 17;
+        int result2 = tester.subtract(x, y);
+
+        if(result2 == 13) {
+            System.out.println("Test Ok");
+        } else {
+            System.out.println("Error!");
+        }
     }
 }
