ba18f50a010c8db013efef5856217cecc1a59ac7
Jacek Podolski <jacekpodolski80@gmail.com>
2022-02-23 18:43:34 +0100

    Immutable user

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,29 +1,8 @@
 package com.kodilla.testing;
-import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
 
 public class TestingMain {
     public static void main(String[] args) {
-        Calculator tester = new Calculator();
 
-        int a = 15;
-        int b = 7;
-        int result1 = tester.add(a, b);
-
-        if(result1 == 22) {
-
-            System.out.println("Test OK");
-        } else {
-            System.out.println("Error!");
-        }
-
-        int x = 30;
-        int y = 17;
-        int result2 = tester.subtract(x, y);
-
-        if(result2 == 13) {
-            System.out.println("Test Ok");
-        } else {
-            System.out.println("Error!");
-        }
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -0,0 +1,11 @@
+package com.kodilla.testing.collection;
+
+import java.util.List;
+
+public class OddNumbersExterminator {
+
+    public List<Integer> exterminate(List<Integer> numbers){
+
+        return numbers;
+    }
+}
